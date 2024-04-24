f36f1c3af014a621a893652c4f3f0c959df48dd3
Winson Baring <winsonbaring10@gmail.com>
2024-04-25 05:31:24 +0800

    comment check error

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from keras.models import load_model
 from tensorflow.keras.models import load_model,Sequential
-from keras.callbacks import ModelCheckpoint, EarlyStopping
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-from camera_input_live import camera_input_live
+# from keras.callbacks import ModelCheckpoint, EarlyStopping
+# from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+# from camera_input_live import camera_input_live
 import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
@@ -11,7 +11,7 @@ from PIL import Image
 
 
 import os
-# import cv2
+import cv2
 import numpy as np
 # def load_images_and_labels(data_dir,label):
 #     images = []
@@ -34,17 +34,17 @@ import numpy as np
 #       # Convert labels to one-hot encoding
 #     return np.array(images), np.array(labels)
 
-# # Path to the directory containing the images
+# Path to the directory containing the images
 # cheetah_label = [0, 0]
 # hyena_label = [1, 0]
 # jaguar_label = [0, 1]
 # tiger_label = [1, 1]
 
 
-hyena_label= [1, 0, 0]
-cheetah_label= [0, 1, 0]
-jaguar_label = [1, 1, 0]
-tiger_label = [0, 0, 1]
+# hyena_label= [1, 0, 0]
+# cheetah_label= [0, 1, 0]
+# jaguar_label = [1, 1, 0]
+# tiger_label = [0, 0, 1]
 
 
 
@@ -69,80 +69,49 @@ st.markdown("<br>", unsafe_allow_html=True)  # Add space below the title
 uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 
-# Load the Keras model
-model = load_model('classification_model.h5')
-model.compile(optimizer='adam',
-              loss='binary_crossentropy',  # Binary cross-entropy loss for multi-label classification
-              metrics=['accuracy'],
-              )
-# Function to make prediction
-def predict(image):
-    # Resize image to match model input shape
-    image = image.resize((100, 100))
-    # Convert image to numpy array
-    image_array = np.array(image)
-    # Normalize pixel values
-    image_array = image_array / 255.0
-    # Add batch dimension
-    image_array = np.expand_dims(image_array, axis=0)
-    # Perform prediction using the model
-    prediction = model.predict(image_array)
-    accuracy = .7
-    st.write('Prediction1:',prediction)
-    st.write('Prediction2:',prediction[[[0]]])
-    st.write('Prediction2:',prediction[[[[0]]]])
-    st.write('Prediction3:',prediction[[[[[0]]]]] )
+# # Load the Keras model
+# model = load_model('classification_model.h5')
+# model.compile(optimizer='adam',
+#               loss='binary_crossentropy',  # Binary cross-entropy loss for multi-label classification
+#               metrics=['accuracy'],
+#               )
+# # Function to make prediction
+# def predict(image):
+#     # Resize image to match model input shape
+#     image = image.resize((100, 100))
+#     # Convert image to numpy array
+#     image_array = np.array(image)
+#     # Normalize pixel values
+#     image_array = image_array / 255.0
+#     # Add batch dimension
+#     image_array = np.expand_dims(image_array, axis=0)
+#     # Perform prediction using the model
+#     prediction = model.predict(image_array)
+#     accuracy = .7
+#     st.write('Prediction1:',prediction)
+#     st.write('Prediction2:',prediction[[[0]]])
+#     st.write('Prediction2:',prediction[[[[0]]]])
+#     st.write('Prediction3:',prediction[[[[[0]]]]] )
     
 
 
 
-# Display the uploaded image
-if uploaded_image is not None:
-    image = Image.open(uploaded_image)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+# # Display the uploaded image
+# if uploaded_image is not None:
+#     image = Image.open(uploaded_image)
+#     st.image(image, caption='Uploaded Image', use_column_width=True)
     
-    # Button to trigger prediction
-    if st.button('Predict'):
-        predict(image)
-else:
-    st.write('No image uploaded yet.')
+#     # Button to trigger prediction
+#     if st.button('Predict'):
+#         predict(image)
+# else:
+#     st.write('No image uploaded yet.')
 
-def process_image(image_array):
-    # Convert the image buffer to a numpy array
-    nparr = np.frombuffer(image_array.getvalue(), np.uint8)
-    cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    processed_image = cv2.resize(cv2_img, (100, 100))
-    processed_image = processed_image / 255.0
-    processed_image = np.expand_dims(processed_image, axis=0)
-    return processed_image
-
-# # Main Streamlit app
-# def main():
-#     st.title("Live Camera Classification")
-
-#     # Display the camera input
-#     img_file_buffer = camera_input_live(debounce=500)
-    
-#     # img_file_buffer = st.camera_input("Take a picture")
-
-#     # Check if an image was captured
-#     if img_file_buffer is not None:
-#         st.image(img_file_buffer)
-
-#         # Preprocess the image
-#         processed_image = process_image(img_file_buffer)
-
-#         # Make predictions using the model
-#         predicted = model.predict(processed_image)
-
-#         # Display the predicted classification
-#         st.write("Predicted Classification:", predicted)
-        
-
-# # Run the Streamlit app
-# if __name__ == "__main__":
-#     main()
-
-
-
-
+# def process_image(image_array):
+#     # Convert the image buffer to a numpy array
+#     nparr = np.frombuffer(image_array.getvalue(), np.uint8)
+#     cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+#     processed_image = cv2.resize(cv2_img, (100, 100))
+#     processed_image = processed_image / 255.0
+#     processed_image = np.expand_dims(processed_image, axis=0)
+#     return processed_image
