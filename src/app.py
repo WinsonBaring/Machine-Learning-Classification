2de3da57bbf203bf560708b4d5ca8c7f577b79ce
Winson Baring <winsonbaring10@gmail.com>
2024-04-25 04:00:34 +0800

    fix 2

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from functions import load_images_and_labels 
 from keras.models import load_model
 from tensorflow.keras.models import load_model,Sequential
 from keras.callbacks import ModelCheckpoint, EarlyStopping
@@ -7,13 +6,47 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from camera_input_live import camera_input_live
 import matplotlib.pyplot as plt
 import numpy as np
-# import cv2
 
 import streamlit as st
 from PIL import Image
 
-import os
-# Title
+
+# import os
+# import cv2
+# import numpy as np
+# def load_images_and_labels(data_dir,label):
+#     images = []
+#     labels = []
+
+#     # Iterate through files in the data directory
+#     for filename in os.listdir(data_dir):
+#         # Read the image
+#         image_path = os.path.join(data_dir, filename)
+#         image = cv2.imread(image_path)
+#         print(image_path)
+#         if image is not None:
+#             # Preprocess the image (resize, normalize, etc.)
+#             image = cv2.resize(image, (100, 100))
+#             image = image / 255.0  # Normalize pixel values
+#             # image = np.expand_dims(image, axis=0)
+#             images.append(image)
+#             # Add label (assuming all images are cheetah)
+#             labels.append(label)  # One-hot encoding for "cheetah" label
+#       # Convert labels to one-hot encoding
+#     return np.array(images), np.array(labels)
+
+# Path to the directory containing the images
+# cheetah_label = [0, 0]
+# hyena_label = [1, 0]
+# jaguar_label = [0, 1]
+# tiger_label = [1, 1]
+
+
+hyena_label= [1, 0, 0]
+cheetah_label= [0, 1, 0]
+jaguar_label = [1, 1, 0]
+tiger_label = [0, 0, 1]
+
 
 
 st.markdown("<h1 style='text-align: center;'>The Identity Your Animal</h1>", unsafe_allow_html=True)
