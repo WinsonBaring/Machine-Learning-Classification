ac3a8a9680f0a0dcc9efe5934d09bc28a20e1668
Winson Baring <winsonbaring10@gmail.com>
2024-04-25 05:27:56 +0800

    cv2 fix

--- src/app.py
+++ src/app.py
@@ -11,34 +11,34 @@ from PIL import Image
 
 
 import os
-import cv2
+# import cv2
 import numpy as np
-def load_images_and_labels(data_dir,label):
-    images = []
-    labels = []
+# def load_images_and_labels(data_dir,label):
+#     images = []
+#     labels = []
 
-    # Iterate through files in the data directory
-    for filename in os.listdir(data_dir):
-        # Read the image
-        image_path = os.path.join(data_dir, filename)
-        image = cv2.imread(image_path)
-        print(image_path)
-        if image is not None:
-            # Preprocess the image (resize, normalize, etc.)
-            image = cv2.resize(image, (100, 100))
-            image = image / 255.0  # Normalize pixel values
-            # image = np.expand_dims(image, axis=0)
-            images.append(image)
-            # Add label (assuming all images are cheetah)
-            labels.append(label)  # One-hot encoding for "cheetah" label
-      # Convert labels to one-hot encoding
-    return np.array(images), np.array(labels)
+#     # Iterate through files in the data directory
+#     for filename in os.listdir(data_dir):
+#         # Read the image
+#         image_path = os.path.join(data_dir, filename)
+#         image = cv2.imread(image_path)
+#         print(image_path)
+#         if image is not None:
+#             # Preprocess the image (resize, normalize, etc.)
+#             image = cv2.resize(image, (100, 100))
+#             image = image / 255.0  # Normalize pixel values
+#             # image = np.expand_dims(image, axis=0)
+#             images.append(image)
+#             # Add label (assuming all images are cheetah)
+#             labels.append(label)  # One-hot encoding for "cheetah" label
+#       # Convert labels to one-hot encoding
+#     return np.array(images), np.array(labels)
 
-# Path to the directory containing the images
-cheetah_label = [0, 0]
-hyena_label = [1, 0]
-jaguar_label = [0, 1]
-tiger_label = [1, 1]
+# # Path to the directory containing the images
+# cheetah_label = [0, 0]
+# hyena_label = [1, 0]
+# jaguar_label = [0, 1]
+# tiger_label = [1, 1]
 
 
 hyena_label= [1, 0, 0]
